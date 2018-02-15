c3959166d94a84b963fd0004acf5268ae28011bf
DESKTOP-GPJCRMT\aulas <dsad@dasd.cs>
2018-02-15 11:45:40 +0000

    Ola Mundo!

--- OlaMundo2/Program.cs
+++ OlaMundo2/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OlaMundo2
+namespace OlaMundo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ola Mundo");
+            Console.ReadKey();
         }
     }
 }
